154ee425498f61cccc645376ec87d438a20d35d2
Fernando Lamas <lamas.fernando1419@gmail.com>
2021-04-07 17:53:26 -0300

    Fixed navbar-menu hamburger styles and js

--- js/main.js
+++ js/main.js
@@ -1,21 +1,23 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', function () {
    // Get all "navbar-burger" elements
    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
    // Check if there are any navbar burgers
    if ($navbarBurgers.length > 0) {
       // Add a click event on each of them
       $navbarBurgers.forEach(el => {
-         el.addEventListener('click', () => {
+         el.addEventListener('click', function () {
             // Get the target from the "data-target" attribute
-            const target = el.dataset.target;
-            const $target = document.getElementById(target);
+            const target = el.dataset.target; // navbarMain
+            // console.log(target);
+            const menu = document.getElementById(target);
+            // console.log(menu);
+
             // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
             el.classList.toggle('is-active');
-            $target.classList.toggle('is-active');
+            menu.classList.toggle('is-active');
          });
       });
    }
-
 });
 
 document.querySelector("#contactform").reset(); // Resets contact form data.
